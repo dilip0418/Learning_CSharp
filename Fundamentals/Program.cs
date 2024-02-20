c07820a58bee30a03231e4d1e0c165215303583b
Dilip Kumar B K <dilip@tegworld.net>
2024-02-20 14:31:44 +0530

    Simple Inheritance

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -4,157 +4,157 @@ using PATB = ProjectA.TeamB;
 //using ProjectA.TeamA;
 //using ProjectA.TeamB;
 
-class Program
-{
-    private static void Main(string[] args)
-    {
+//class Program
+//{
+//    private static void Main(string[] args)
+//    {
 
-        //Writng to the console
-        Console.WriteLine("Hello, World!");
+//        //Writng to the console
+//        Console.WriteLine("Hello, World!");
 
-        //Console.WriteLine("Enter roll No:");
+//        //Console.WriteLine("Enter roll No:");
 
-        ////Reading from the console
-        //int roll = int.Parse(Console.ReadLine());
+//        ////Reading from the console
+//        //int roll = int.Parse(Console.ReadLine());
 
-        //Console.WriteLine("Enter name:");
-        //string name = Console.ReadLine();
+//        //Console.WriteLine("Enter name:");
+//        //string name = Console.ReadLine();
 
-        //Console.WriteLine("Enter stream");
-        //string stream = Console.ReadLine();
-        ////var name = Console.ReadLine();
+//        //Console.WriteLine("Enter stream");
+//        //string stream = Console.ReadLine();
+//        ////var name = Console.ReadLine();
 
-        ////Using placeholder syntax.
-        ////Console.WriteLine("Hi {0}, nice meeting you!", name);
+//        ////Using placeholder syntax.
+//        ////Console.WriteLine("Hi {0}, nice meeting you!", name);
 
-        ////Using sting concatenation syntax.
-        ////Console.WriteLine("Hi " + name + ", nice meeting you!");
+//        ////Using sting concatenation syntax.
+//        ////Console.WriteLine("Hi " + name + ", nice meeting you!");
 
-        //Variables c = new Variables(roll, name, stream);
-        //Console.WriteLine(c.ToString());
-
-
-        //Operators op1 = new Operators(10, 20);
-        ////Operators op2 = new Operators(10,20);
-        //Operators op2 = new Operators(10, 30);
-
-        //Console.WriteLine(op1.Equals(op2));
-
-        //TypeConversion
-        //TypeConversion.implicitTypeConversion();
-        //TypeConversion.explicitTypeConversion();
+//        //Variables c = new Variables(roll, name, stream);
+//        //Console.WriteLine(c.ToString());
 
 
-        //Conditionals -[if,if-else, if-elseif]
-        //Conditionals cond = new Conditionals();
-        //cond.ifBlock();
-        //cond.ifElseIFBLock();
+//        //Operators op1 = new Operators(10, 20);
+//        ////Operators op2 = new Operators(10,20);
+//        //Operators op2 = new Operators(10, 30);
 
-        //Conditionals -[switch, break]
-        //cond.SwitchStatement();
+//        //Console.WriteLine(op1.Equals(op2));
 
-
-        //Loops
-        /*
-        Loops loop = new Loops();
+//        //TypeConversion
+//        //TypeConversion.implicitTypeConversion();
+//        //TypeConversion.explicitTypeConversion();
 
 
-        //loop.whileLoop(1234);
-        //loop.doWhileLoop();
-        //loop.forLoop();
-        //loop.forEachLoop();
-        int start = int.Parse(Console.ReadLine());
-        int end = int.Parse(Console.ReadLine());
-        for(int i = start; i <= end; i++)
-            if (loop.isPrime(i))
-                Console.WriteLine(i);
+//        //Conditionals -[if,if-else, if-elseif]
+//        //Conditionals cond = new Conditionals();
+//        //cond.ifBlock();
+//        //cond.ifElseIFBLock();
 
-    */
-
-        /*
-            * 
-        Methods in C#
-
-        string Name = Console.ReadLine();
-
-        //Invoking static method using class
-        Methods.printNameStaticType(Name);
-
-        //Invoking instance method using an instance/object of the class
-        Methods method = new Methods();
-        method.printNameInstanceType(Name);
-        */
+//        //Conditionals -[switch, break]
+//        //cond.SwitchStatement();
 
 
-        // Method Parameter Types
-        /*
-        MethodParameters mp = new MethodParameters();
-        //Value parameter
-        mp.printNumber(10);
-
-        //Reference Parameter
-        int i = 0;
-        Console.WriteLine("The value of i before function call is: {0}", i);
-        mp.changeNumber(ref i);
-        Console.WriteLine("The value of i after function call is: {0}", i);
-
-        //out parameters
-        int sum = 0, product = 0;
-        mp.Calculate(10, 20, out sum, out product);
-        Console.WriteLine("Sum = {0} and Product = {1}", sum, product);
-
-        //Parameter Arrays
-        int[] arr = { 1, 2, 3, 4 };
-        mp.printNumbers(arr);
-
-        //Invoking printNumbers method with passing arguements to it.
-        mp.printNumbers(); //if the method definition has the parameters prefixed with params keyword then we can invoke that method with/without passing the arguments prefixed with params keyword and always the paramenters must be mentioned after all the other parameters
-     */
-
-        //namespaces
-
-        //Problem that arises if you don't use namespaces
-        // ClassA.print();  Here the compiler get's confused on which class to be chosen to run because of the existence of class in both the namespaces with same name [ClassA]
+//        //Loops
+//        /*
+//        Loops loop = new Loops();
 
 
-        //Calling a function present in ProjectA.TeamA namespace
-        //using fully qualified syntax
+//        //loop.whileLoop(1234);
+//        //loop.doWhileLoop();
+//        //loop.forLoop();
+//        //loop.forEachLoop();
+//        int start = int.Parse(Console.ReadLine());
+//        int end = int.Parse(Console.ReadLine());
+//        for(int i = start; i <= end; i++)
+//            if (loop.isPrime(i))
+//                Console.WriteLine(i);
 
-        //ProjectA.TeamA.ClassA.print();
-        //ProjectA.TeamB.ClassA.print();
+//    */
 
-        //using aliases
+//        /*
+//            * 
+//        Methods in C#
+
+//        string Name = Console.ReadLine();
+
+//        //Invoking static method using class
+//        Methods.printNameStaticType(Name);
+
+//        //Invoking instance method using an instance/object of the class
+//        Methods method = new Methods();
+//        method.printNameInstanceType(Name);
+//        */
+
+
+//        // Method Parameter Types
+//        /*
+//        MethodParameters mp = new MethodParameters();
+//        //Value parameter
+//        mp.printNumber(10);
+
+//        //Reference Parameter
+//        int i = 0;
+//        Console.WriteLine("The value of i before function call is: {0}", i);
+//        mp.changeNumber(ref i);
+//        Console.WriteLine("The value of i after function call is: {0}", i);
+
+//        //out parameters
+//        int sum = 0, product = 0;
+//        mp.Calculate(10, 20, out sum, out product);
+//        Console.WriteLine("Sum = {0} and Product = {1}", sum, product);
+
+//        //Parameter Arrays
+//        int[] arr = { 1, 2, 3, 4 };
+//        mp.printNumbers(arr);
+
+//        //Invoking printNumbers method with passing arguements to it.
+//        mp.printNumbers(); //if the method definition has the parameters prefixed with params keyword then we can invoke that method with/without passing the arguments prefixed with params keyword and always the paramenters must be mentioned after all the other parameters
+//     */
+
+//        //namespaces
+
+//        //Problem that arises if you don't use namespaces
+//        // ClassA.print();  Here the compiler get's confused on which class to be chosen to run because of the existence of class in both the namespaces with same name [ClassA]
+
+
+//        //Calling a function present in ProjectA.TeamA namespace
+//        //using fully qualified syntax
+
+//        //ProjectA.TeamA.ClassA.print();
+//        //ProjectA.TeamB.ClassA.print();
+
+//        //using aliases
        
-        //PATA.ClassA.print();
-        //PATB.ClassA.print();
+//        //PATA.ClassA.print();
+//        //PATB.ClassA.print();
 
-        //We can separate the namespaces into separate assemblies/projects
+//        //We can separate the namespaces into separate assemblies/projects
 
-        PATA.ClassA.print();
-        PATB.ClassA.print();
+//        PATA.ClassA.print();
+//        PATB.ClassA.print();
 
 
-    }
-}
-
-//namespace ProjectA.TeamA
-//{
-//    class ClassA
-//    {
-//        public static void print()
-//        {
-//            Console.WriteLine("Team A print method...!");
-//        }
 //    }
 //}
 
-//namespace ProjectA.TeamB
-//{
-//    class ClassA
-//    {
-//        public static void print()
-//        {
-//            Console.WriteLine("Team B print method...!");
-//        }
-//    }
-//}
+////namespace ProjectA.TeamA
+////{
+////    class ClassA
+////    {
+////        public static void print()
+////        {
+////            Console.WriteLine("Team A print method...!");
+////        }
+////    }
+////}
+
+////namespace ProjectA.TeamB
+////{
+////    class ClassA
+////    {
+////        public static void print()
+////        {
+////            Console.WriteLine("Team B print method...!");
+////        }
+////    }
+////}
---
+++ OOPS/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Classes in C#
+class Person
+{
+    private string _firstName;
+
+    //FirstName is a public property that provides access to the _firstName field.
+    //The get accessor returns the value of the private field _firstName.
+    //The set accessor sets the value of the private field _firstName to the provided value.
+    public string FirstName
+    {
+        get
+        {
+            return _firstName;
+        }
+        set
+        {
+            _firstName = value;
+        }
+    }
+
+    //shorthand form of adding getter and setter ot the fields if there public
+    public string _lastName { get; set; }
+
+
+    public short _age { get; set; }
+    public string _nationality { get; set; }
+
+    //Default constructor
+    public Person() { }
+
+    public Person(string FirstName, string LastName)
+    {
+        this._firstName = FirstName;
+        this._lastName = LastName;
+    }
+
+    public override string ToString()
+    {
+        return "First Name : " + this.FirstName + "\nLast Name : " + this._lastName + "\nAge : " + this._age + "\nNationality : " + this._nationality;
+    }
+
+}
+
+//Static and instance members in C#
+class Circle
+{
+    private int _radius;
+    private static float _PI = 3.142F;
+
+    public Circle(int Radius)
+    {
+        this._radius = Radius;
+    }
+
+    public float CalculateArea()
+    {
+        return _PI * this._radius * this._radius;
+    }
+}
+
+//Inheitance in C#
+class Employee
+{
+    public string _firstName;
+    public string _lastName;
+
+    //parameterized constructor
+    public Employee(string FirstName, string LastName)
+    {
+        this._firstName = FirstName;
+        this._lastName = LastName;
+    }
+
+    //Whenever a default constructor is   
+    public Employee() : base() { }
+
+    public void printFullName()
+    {
+        Console.Write(this._firstName + " " + this._lastName);
+    }
+}
+
+class FullTimeEMployee : Employee
+{
+    public float monthlySalary;
+
+    //using the base keyword to invoke a particular base class constructor - base refers to the base/parent class members
+    //the below way of syntax lets the user to optional pass the parameters while creating the objects. If we miss out any params the default values for the missed parameters will be assigned to them
+    public FullTimeEMployee(string fName = "No First Name", string lName = "No Last Name", float salary = 0.0F) : base(fName, lName)
+    {
+        this.monthlySalary = salary;
+    }
+
+    public void printFTEDetails()
+    {
+        base.printFullName();
+        Console.WriteLine(" " + this.monthlySalary);
+    }
+
+}
+
+class PartTimeEMployee : Employee
+{
+    float hourlySalary;
+
+
+    public PartTimeEMployee(string fName = "No First Name", string lName = "No Last Name", float salary = 0.0F) : base(fName, lName)
+    {
+        this.hourlySalary = salary;
+    }
+
+    public void printPTEDetails()
+    {
+        base.printFullName();
+        Console.WriteLine(" " + this.hourlySalary);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Person person = new Person()
+        //{
+        //    FirstName = "Dilip",
+        //    _lastName = "Kumar B K",
+        //    _age = 23,
+        //    _nationality = "Indian"
+        //};
+        //Console.WriteLine("Person Details:\n" + person);
+
+        //Circle circle1 = new Circle(10);
+        //float area1 = circle1.CalculateArea();
+        //Console.WriteLine("The area of the circle is : {0}", area1);
+
+        FullTimeEMployee FTE = new FullTimeEMployee("Dilip", "Kumar B K", 50000);
+        //FTE.printFullName();
+
+        FTE.printFTEDetails();
+
+        PartTimeEMployee PTE = new PartTimeEMployee("Shivesh", "V G", 1500);
+        PTE.printPTEDetails();
+
+        //In the below object the salary argument isn't passed so it'll take default value 0.0F
+        FullTimeEMployee FTE1 = new FullTimeEMployee("Ganesh","Chitriki");
+
+        FTE1.printFTEDetails();
+
+    }
+}
+
+
