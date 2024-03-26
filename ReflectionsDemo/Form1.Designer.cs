06dff31b01b743293bf093d9a2da4a5f9df1709e
Dilip Kumar B K <dilip@tegworld.net>
2024-03-26 11:40:51 +0530

    Reflections Demo using WF

---
+++ ReflectionsDemo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ReflectionsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstProperties = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstMethods = new System.Windows.Forms.ListBox();
+            this.lstConstructors = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstProperties
+            // 
+            this.lstProperties.FormattingEnabled = true;
+            this.lstProperties.HorizontalScrollbar = true;
+            this.lstProperties.Location = new System.Drawing.Point(12, 91);
+            this.lstProperties.Name = "lstProperties";
+            this.lstProperties.Size = new System.Drawing.Size(233, 277);
+            this.lstProperties.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstMethods
+            // 
+            this.lstMethods.FormattingEnabled = true;
+            this.lstMethods.HorizontalScrollbar = true;
+            this.lstMethods.Location = new System.Drawing.Point(264, 91);
+            this.lstMethods.Name = "lstMethods";
+            this.lstMethods.Size = new System.Drawing.Size(233, 277);
+            this.lstMethods.TabIndex = 0;
+            // 
+            // lstConstructors
+            // 
+            this.lstConstructors.FormattingEnabled = true;
+            this.lstConstructors.HorizontalScrollbar = true;
+            this.lstConstructors.Location = new System.Drawing.Point(515, 91);
+            this.lstConstructors.Name = "lstConstructors";
+            this.lstConstructors.Size = new System.Drawing.Size(233, 277);
+            this.lstConstructors.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 411);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstConstructors);
+            this.Controls.Add(this.lstMethods);
+            this.Controls.Add(this.lstProperties);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstProperties;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstMethods;
+        private System.Windows.Forms.ListBox lstConstructors;
+    }
+}
+
---
+++ ReflectionsDemo/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace ReflectionsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstProperties.Items.Clear();
+            lstMethods.Items.Clear();
+            lstConstructors.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text;
+            Type T = Type.GetType(searchText);
+
+            lstMethods.Items.Clear();
+            lstProperties.Items.Clear();
+            lstConstructors.Items.Clear();
+
+
+            PropertyInfo[] properties = T.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                lstProperties.Items.Add(property);
+            }
+
+            MethodInfo[] methods = T.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                lstMethods.Items.Add(method);
+            }
+
+            ConstructorInfo[] constructors = T.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                lstConstructors.Items.Add(constructor);
+            }
+
+        }
+    }
+}
